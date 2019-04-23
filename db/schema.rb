4c09b3441a37064a123f78c8540fe3b83e2d6063
James-NewtonIII <jacenewt57@yahoo.com>
2019-04-23 17:19:37 +0000

    added a quantity to check for budgetApprovers

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_23_010117) do
+ActiveRecord::Schema.define(version: 2019_04_23_170555) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "quantity", default: 0
   end
 
   create_table "items", force: :cascade do |t|
