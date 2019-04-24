d55c8c3db93c340c189542454e898b30874a8acf
MichaelMontgom <memontgomery@valdosta.edu>
2019-04-24 18:18:53 -0400

    changed line items to show in descending order

--- app/controllers/company_controller.rb
+++ app/controllers/company_controller.rb
@@ -1,6 +1,6 @@
 class CompanyController < ApplicationController
   def index
-    @items = Item.order(:amount)
+    @items = Item.order("amount DESC")
     @request_items = RequestItem.order(:amount)
     @taf_items = TafItem.order(:expense_date)
   end
