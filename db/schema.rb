b4efe8895caddd8aa58f4a6f1d0f93951b626285
James-NewtonIII <jacenewt57@yahoo.com>
2019-04-24 20:32:13 +0000

    Fixed the shit

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2019_04_24_032352) do
-
+ActiveRecord::Schema.define(version: 2019_04_24_193259) do
 
   create_table "budget_approvers", force: :cascade do |t|
     t.string "name"
