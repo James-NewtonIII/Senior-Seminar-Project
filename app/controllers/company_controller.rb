f6112c04a3f7c1336c8d1c357239472988e0355c
James-NewtonIII <jacenewt57@yahoo.com>
2019-05-01 19:55:50 +0000

    specific tafs show up

--- app/controllers/company_controller.rb
+++ app/controllers/company_controller.rb
@@ -6,19 +6,25 @@ class CompanyController < ApplicationController
     if ( account_signed_in? )
       if (current_account.accountable_type=="Employee")
         @employee = Employee.find(current_account.accountable_id)
-        @tafs = Taf.where(employee_id: @employee.id)
-        @carts = Cart.where(employee_id: @employee.id)
-      elsif (current_account.accountable_type=="BudgetApprover")
-        @budget_approver = BudgetApprover.find(current_account.accountable_id)
-        @taf_items = TafItem.where(dept: @budget_approver.department_id)
-        @items = Item.where(dept: @budget_approver.department_id)
-      else
+       # @tafs = Taf.where(employee_id: @employee.id)
+        #@carts = Cart.where(employee_id: @employee.id)
         @tafs = Taf.all
         @carts = Cart.all
 
         @taf_items = TafItem.all
         @items = Item.all
-      end
+      elsif (current_account.accountable_type=="BudgetApprover")
+        @budget_approver = BudgetApprover.find(current_account.accountable_id)
+        @taf_items = TafItem.where(dept: @budget_approver.department_id)
+        @items = Item.where(dept: @budget_approver.department_id)
+        else
+        @tafs = Taf.all
+        @carts = Cart.all
+
+        @taf_items = TafItem.all
+        @items = Item.all
+    end
+      
     end
    
     
