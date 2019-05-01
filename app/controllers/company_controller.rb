fc4ff333bb105c5ed19ecadf267ecb6508425420
Robert De La Cruz II <robdlcii@gmail.com>
2019-04-30 21:59:36 -0400

    Worked on the filtering logic for budget approvers dash view

--- app/controllers/company_controller.rb
+++ app/controllers/company_controller.rb
@@ -3,11 +3,17 @@ class CompanyController < ApplicationController
   before_action :set_current_taf, only: [:new, :create]
 
   def index
-    if (current_account.accountable_type=="Employee")
-      @employee = Employee.find(current_account.accountable_id)
-      @tafs = Taf.where(employee_id: @employee.id)
+    if ( account_signed_in? )
+      if (current_account.accountable_type=="Employee")
+        @employee = Employee.find(current_account.accountable_id)
+        @tafs = Taf.where(employee_id: @employee.id)
+      elsif (current_account.accountable_type=="BudgetApprover")
+        @budget_approver = BudgetApprover.find(current_account.accountable_id)
+        puts @current_account.accountable_id
+        @taf_items = TafItem.where(dept: @budget_approver.department_id)
+      end
+    end
     else
       @tafs = Taf.all
-    end 
   end
 end
