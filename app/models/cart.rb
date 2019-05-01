9d06d1f9cc71342fcedf73f95548821b3475cc45
James-NewtonIII <jacenewt57@yahoo.com>
2019-05-01 14:56:31 +0000

    yeeeeeee

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,7 +1,20 @@
 class Cart < ApplicationRecord
-    has_many :items, dependent: :destroy
-    belongs_to :employee
-    belongs_to :budget_approver, optional: true
-    belongs_to :payment_manager, optional: true
-    accepts_nested_attributes_for :items, reject_if: :all_blank, allow_destroy: true
+   has_many :items, dependent: :destroy
+
+    def add_item(item)
+        current_item = items.build(id: item.id)
+        
+        current_item
+    end
+
+    def remove_item(item)
+        item = item.find_by(item_id: item.id)
+        item.quantity -= 1
+        
+        item
+    end
+
+    def total_expected_amount
+        item.to_a.sum { |item| item.amount }
+    end
 end
